44e758740f1155bf2ae6731083393cb0a2fdbedd
nivancheva <nedyalka.tsintsarova@gmail.com>
2024-03-01 13:46:47 +0200

    add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
+      <button class="btn btn-outline-primary btn-sm">Test</button>
     </div>
   </header>
 
