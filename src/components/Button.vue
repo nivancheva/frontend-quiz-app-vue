2d78f0f132ca8589945b5c77e69fed93e0417407
nivancheva <nedyalka.tsintsarova@gmail.com>
2024-04-17 16:14:04 +0300

    basic styles

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,15 +1,19 @@
 <script setup>
+import { reactive, computed } from 'vue'
 const props = defineProps({
   title: String,
   icon: String,
 })
+
 
 </script>
 
 <template>
 
  <button class="btn btn-primary d-flex align-items-center gap-3 btn-quiz">
-    <img :src="`src/assets/images/` + icon + `.svg`"/>
+    <div :class='["icon", `icon-${title}`]'>
+        <img :src="`src/assets/images/` + icon + `.svg`" :alt="title"/>
+    </div>
     <p>{{title}}</p>
 </button>
 
@@ -21,5 +25,27 @@ const props = defineProps({
     border: none;
     color: var(--clr-text);
     font-weight: bold;
-  }
+}
+
+.icon {
+    padding: .5rem;
+    border-radius: .5rem;
+}
+
+.icon-HTML {
+    background: var(--clr-red-a);
+}
+
+.icon-CSS {
+    background: var(--clr-green-a);
+}
+
+.icon-JavaScript {
+    background: var(--clr-blue-a);
+}
+
+.icon-Accessibility {
+    background: var(--clr-purple-a);
+}
+
 </style>
