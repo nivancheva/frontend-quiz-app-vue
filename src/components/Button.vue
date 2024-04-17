2cc1921a5ae02b11a45e92f2828dd7b1cafec209
nivancheva <nedyalka.tsintsarova@gmail.com>
2024-04-17 15:34:11 +0300

    add Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps({
+  title: String,
+  icon: String,
+})
+
+</script>
+
+<template>
+
+ <button class="btn btn-primary d-flex align-items-center gap-3 btn-quiz">
+    <img :src="`src/assets/images/` + icon + `.svg`"/>
+    <p>{{title}}</p>
+</button>
+
+</template>
+
+<style scoped>
+.btn-quiz {
+    background: var(--clr-bg-buttons);
+    border: none;
+    color: var(--clr-text);
+    font-weight: bold;
+  }
+</style>
