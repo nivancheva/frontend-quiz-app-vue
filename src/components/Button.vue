a3feb8968f05ae11efdd2206e2b9014a40abe127
nivancheva <nedyalka.tsintsarova@gmail.com>
2024-11-20 14:15:36 +0200

    btns bg

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -11,7 +11,7 @@ const props = defineProps({
 
 <template>
 
- <router-link :to="{name: url}" class="d-flex align-items-center gap-3 btn-quiz">
+ <router-link :to="{name: url}" :class='["d-flex align-items-center gap-3 btn-quiz", `btn-${title}`]'>
     <div :class='["bg-option", `icon-${title}`]'>
         <img :src="`src/assets/images/` + icon + `.svg`" :alt="title"/>
     </div>
@@ -36,6 +36,26 @@ const props = defineProps({
 
 .icon-Accessibility {
     background: var(--clr-a);
+}
+
+.btn-HTML:hover,
+.btn-HTML:focus {
+    background: var(--clr-html-bg);
+}
+
+.btn-CSS:hover,
+.btn-CSS:focus {
+    background: var(--clr-css-bg);
+}
+
+.btn-JavaScript:hover,
+.btn-JavaScript:focus {
+    background: var(--clr-js-bg);
+}
+
+.btn-Accessibility:hover,
+.btn-Accessibility:focus {
+    background: var(--clr-a-bg);
 }
 
 </style>
